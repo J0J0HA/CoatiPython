17354762e043b5778161225582d4f7ce6f57675e
J0J0HA <huhndietlinde@gmail.com>
2022-09-24 22:38:19 +0200

    Disabled automated welcome page

--- coati.js
+++ coati.js
@@ -699,6 +699,7 @@ $(() => {
   var welcome = localStorage.getItem("welcome");
   var thd = 1000 * 60 * 60 * 24 * 30;
   if ((!welcome) || (welcome < Date.now() - thd)) {
+    return main();
     if (window.location.hash == "#welcome") {
       window.location.hash = "#";
       localStorage.setItem("welcome", Date.now());
